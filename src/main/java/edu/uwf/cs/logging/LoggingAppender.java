880de4cf9128407a2a2b6060c155a2b1d2e68edc
Bryan Solomon <bryansolomon@Bryans-MacBook-Pro.local>
2017-04-22 17:20:35 -0700

    Tidying up custom appender

--- src/main/java/edu/uwf/cs/logging/LoggingAppender.java
+++ src/main/java/edu/uwf/cs/logging/LoggingAppender.java
@@ -8,12 +8,13 @@ import static org.apache.log4j.Priority.*;
 
 public class LoggingAppender extends ConsoleAppender {
 
+    /* keep commented out colors just in case someone wants to change to them */
     private static final int NORMAL = 0;
-    private static final int BRIGHT = 1;
-    private static final int FOREGROUND_RED = 31;
-    private static final int FOREGROUND_GREEN = 32;
+//    private static final int BRIGHT = 1;
+//    private static final int FOREGROUND_RED = 31;
+//    private static final int FOREGROUND_GREEN = 32;
     private static final int FOREGROUND_YELLOW = 33;
-    private static final int FOREGROUND_BLUE = 34;
+//    private static final int FOREGROUND_BLUE = 34;
     private static final int FOREGROUND_CYAN = 36;
 
     private static final String PREFIX = "\u001b[";
@@ -21,18 +22,10 @@ public class LoggingAppender extends ConsoleAppender {
     private static final char SEPARATOR = ';';
     private static final String END_COLOUR = PREFIX + SUFFIX;
 
-//    private static final String FATAL_COLOUR = PREFIX
-//            + BRIGHT + SEPARATOR + FOREGROUND_RED + SUFFIX;
-//    private static final String ERROR_COLOUR = PREFIX
-//            + NORMAL + SEPARATOR + FOREGROUND_RED + SUFFIX;
     private static final String WARN_COLOUR = PREFIX
             + NORMAL + SEPARATOR + FOREGROUND_CYAN + SUFFIX;
     private static final String INFO_COLOUR = PREFIX
             + NORMAL + SEPARATOR + FOREGROUND_YELLOW + SUFFIX;
-//    private static final String DEBUG_COLOUR = PREFIX
-//            + NORMAL + SEPARATOR + FOREGROUND_CYAN + SUFFIX;
-//    private static final String TRACE_COLOUR = PREFIX
-//            + NORMAL + SEPARATOR + FOREGROUND_BLUE + SUFFIX;
 
     /**
      * Wraps the ANSI control characters around the
@@ -54,16 +47,10 @@ public class LoggingAppender extends ConsoleAppender {
      */
     private String getColour(Level level) {
         switch (level.toInt()) {
-//            case FATAL_INT:
-//                return FATAL_COLOUR;
-//            case ERROR_INT:
-//                return ERROR_COLOUR;
             case WARN_INT:
                 return WARN_COLOUR;
             case INFO_INT:
                 return INFO_COLOUR;
-//            case DEBUG_INT:
-//                return DEBUG_COLOUR;
             default:
                 return INFO_COLOUR;
         }
